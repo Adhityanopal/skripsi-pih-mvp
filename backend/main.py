bbe605e100e653ca63165a2ad752a5b1d495fdf9
Adhitya <adhitya.naufal21@mhs.uinjkt.ac.id>
2026-02-24 09:38:08 +0700

    add config vercel

--- backend/main.py
+++ backend/main.py
@@ -80,19 +80,12 @@ class TaskReview(SQLModel):
 # --- INISIALISASI APLIKASI ---
 app = FastAPI(title="API Sistem Manajemen Kinerja Tim Media PIH")
 
-app = FastAPI()
-
-origins = [
-    "http://localhost:5173",          # Untuk testing lokal
-    "https://pih-frontend-nine.vercel.app", 
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],            
+    allow_origins=["*"], # Ini kunci biar gak CORS error lagi
     allow_credentials=True,
-    allow_methods=["*"],             
-    allow_headers=["*"],             
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 genai.configure(api_key=settings.GEMINI_API_KEY)
